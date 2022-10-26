f1d4cbdf9958c7febfba224cc52d176ca8af8f16
CHANG180-PC\chang <chang180@gmail.com>
2022-10-26 23:41:01 +0800

    adjust

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import path from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    base:'./',
+  resolve: {
+    alias: {'@/': `${path.resolve(__dirname, '/')}/`}
+  }
+})
